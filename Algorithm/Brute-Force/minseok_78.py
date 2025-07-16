293dc946ab1cfa4797bf8309fd36e253957aa36b
Luka <minseok6415@gmail.com>
2025-07-16 23:45:50 +0900

    Add another solution to minseok_78.py

--- Algorithm/Brute-Force/minseok_78.py
+++ Algorithm/Brute-Force/minseok_78.py
@@ -16,3 +16,16 @@ class Solution:
         backtrack(0)
 
         return result
+    
+#------------------------------------------------------------------------
+
+from itertools import combinations
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:        
+        result = [[]]   
+        
+        for case_num in range(1, len(nums)+1):
+            # combinations(iterable, n) -> iterable 변수에서 n개를 뽑는 모든 경우의 수를 iterable 형태로 반환함
+            result.extend(combinations(nums, case_num))
+        
+        return result
