6af223648f2e37436b75e6413556a752f9e0e1fa
mintaeyoon <136074046+mintaeyoon@users.noreply.github.com>
2025-08-10 22:26:15 +0900

    Create taeyoon_733.py

---
+++ Algorithm/BFS_DFS/taeyoon_733.py
@@ -0,0 +1,23 @@
+class Solution:
+    def floodFill(self, image, sr, sc, color):
+        rows, cols = len(image), len(image[0])
+        original_color = image[sr][sc]
+        
+        if original_color == color:
+            return image
+        
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols:
+                return
+            if image[r][c] != original_color:
+                return
+            
+            image[r][c] = color
+            
+            dfs(r-1, c)
+            dfs(r+1, c)
+            dfs(r, c-1)
+            dfs(r, c+1)
+        
+        dfs(sr, sc)
+        return image
