290ebaad574d08eb81c1a521a1c514972bbd03d0
mintaeyoon <136074046+mintaeyoon@users.noreply.github.com>
2025-08-20 00:17:54 +0900

    Create taeyoon_743.py

---
+++ Algorithm/ChwaeDanGyungRo/taeyoon_743.py
@@ -0,0 +1,26 @@
+from typing import List
+import heapq
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        g = [[] for _ in range(n + 1)]
+        for u, v, w in times:
+            g[u].append((v, w))
+        
+        INF = 10**18
+        dist = [INF] * (n + 1)
+        dist[k] = 0
+        pq = [(0, k)]
+        
+        while pq:
+            t, u = heapq.heappop(pq)
+            if t > dist[u]:
+                continue
+            for v, w in g[u]:
+                nt = t + w
+                if nt < dist[v]:
+                    dist[v] = nt
+                    heapq.heappush(pq, (nt, v))
+        
+        ans = max(dist[1:])
+        return -1 if ans == INF else ans
